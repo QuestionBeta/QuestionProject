c4464c561b7e5579b3c231c988b08a15b03140b4
Jack <1967270996@qq.com>
2014-12-09 11:40:30 +0800

    更新版本库

---
+++ WebUIFunction/UserFun/MenuFacade.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataBase.AppData;
+
+namespace WebUIFunction
+{
+    public partial class MenuFun
+    {
+        //自定义函数
+
+        //获取菜单列表
+        public List<Menu> GetMenuList(int? id)
+        {
+            if (id == null)
+            {
+                return null;
+            }
+
+            return this.baseDataModel.GetDataListByRoleId(id);
+        }
+
+        /// <summary>
+        /// 获取所有父节点
+        /// </summary>
+        /// <returns></returns>
+        public List<Menu> GetParentNodes()
+        {
+            return this.baseDataModel.GetParentNodes();
+        }
+
+        /// <summary>
+        /// 获取所有子节点
+        /// </summary>
+        /// <returns></returns>
+        public List<Menu> GetChildrenNodes(int? pid)
+        {
+            if (pid == null)
+            {
+                return null;
+            }
+
+            return this.baseDataModel.GetChildrenNodes(pid);
+        }
+    }
+}
